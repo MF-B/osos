aaaabb5a03ebc0cfae15a1daa3a3c1abeefb303d
mf1bzz <guzemin@hotmail.com>
2025-06-08 04:15:43 +0800

    [fix]: 修复共享库问题

--- core/src/mm.rs
+++ core/src/mm.rs
@@ -158,21 +158,32 @@ pub fn load_user_app(
                 .map_err(|_| AxError::InvalidData)?,
         )?;
 
-        if interp_path == "/lib/ld-linux-riscv64-lp64.so.1"
+        if interp_path == "/lib/ld-linux-riscv64-lp64.so.1"  
+            || interp_path == "/lib64/ld-linux-loongarch-lp64d.so.1"  
+            || interp_path == "/lib64/ld-linux-x86-64.so.2"  
+            || interp_path == "/lib/ld-linux-aarch64.so.1"
             || interp_path == "/lib/ld-linux-riscv64-lp64d.so.1"
             || interp_path == "/lib/ld-musl-riscv64-sf.so.1"
-            || interp_path == "/lib/ld-musl-riscv64.so.1"
-            || interp_path == "/lib/ld-musl-x86_64.so.1"
-            || interp_path == "/lib/ld-musl-loongarch-lp64d.so.1"
-            || interp_path == "/lib/ld-musl-aarch64.so.1"
-
-            || interp_path == "/lib64/ld-linux-loongarch-lp64d.so.1"
-            || interp_path == "/lib64/ld-linux-x86-64.so.2"
-            || interp_path == "/lib/ld-linux-aarch64.so.1"
-        {
-            // TODO: Use soft link
-            interp_path = String::from("/musl/lib/libc.so");
+            || interp_path == "/lib64/ld-musl-loongarch-lp64d.so.1"
+        {  
+            interp_path = String::from("/musl/lib/libc.so");  
         }
+
+        // if interp_path == "/lib/ld-linux-riscv64-lp64.so.1"  
+        //     || interp_path == "/lib64/ld-linux-loongarch-lp64d.so.1"  
+        //     || interp_path == "/lib64/ld-linux-x86-64.so.2"  
+        //     || interp_path == "/lib/ld-linux-aarch64.so.1"
+        //     || interp_path == "/lib/ld-glibc-riscv64-sf.so.1"
+        //     || interp_path == "/lib64/ld-glibc-loongarch-lp64d.so.1"
+        // {  
+        //     interp_path = String::from("/musl/lib/libc.so");  
+        // }
+
+        // if interp_path == "/lib64/ld-linux-loongarch-lp64d.so.1"
+        //     || interp_path == "/lib/ld-linux-loongarch-lp64d.so.1"
+        // {
+        //     interp_path = String::from("/glibc/lib/ld-linux-loongarch-lp64d.so.1");
+        // }
 
         // Set the first argument to the path of the user app.
         let mut new_args = vec![interp_path];
